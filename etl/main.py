561bd83bfec3d58865657f068891ec538a1cc148
h6233239-arch <h6233239@gmail.com>
2025-10-30 20:20:05 +0700

    Update main.py

--- etl/main.py
+++ etl/main.py
@@ -6,13 +6,13 @@ from load import load_data
 def main():
     # Проверяем наличие аргумента
     if len(sys.argv) < 2:
-        print("❌ Ошибка: необходимо передать хотя бы 1 аргумент (Google Drive file ID).")
+        print(" Ошибка: необходимо передать хотя бы 1 аргумент (Google Drive file ID).")
         print("Пример: python etl/main.py 1e_B0JuGIwMeVWKbchUdzw8xcAkh0hPbX")
         sys.exit(1)
 
     # Получаем file_id из аргумента командной строки
     file_id = sys.argv[1].strip()
-    print(f"🟢 Получен file_id: {file_id}")
+    print(f" Получен file_id: {file_id}")
 
     # 1️⃣ Extract
     df_raw = extract_data(file_id)
@@ -23,7 +23,7 @@ def main():
     # 3️⃣ Load
     load_data(df_clean)
 
-    print("🎯 Процесс ETL успешно завершён!")
+    print(" Процесс ETL успешно завершён!")
 
 if __name__ == "__main__":
     main()
